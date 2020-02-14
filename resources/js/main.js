ba6b771675a487291a0a7a46077dd6eb93f46ffc
AariyanApu <aariyan.apu@gmail.com>
2020-02-15 01:51:49 +0600

    finally complite hero page

---
+++ resources/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    
+    
+    
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeInLeft');
+    }, {
+        offset: '70%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInRight');
+    }, {
+        offset: '70%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeInUp');
+    }, {
+        offset: '70%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '70%'
+    });
+    
+});
+
