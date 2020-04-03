97100b74bb7dcc75d80ddd432713e8f4ea49c7e7
AariyanApu <aariyan.apu@gmail.com>
2020-04-03 13:05:45 +0600

    animation bug fix

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+
+
     
-    
-    
+ 
     /* Animations on scroll */
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeInLeft');
@@ -26,6 +27,7 @@ $(document).ready(function() {
     }, {
         offset: '30%'
     });
+    
     
 });
 
